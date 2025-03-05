ab3d05e502f4338227d93f40f7e33d7751f60455
Shreyas Rajesh <shreyasrajesh0917@gmail.com>
2025-03-05 01:35:27 -0800

    Setup prototype done

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,42 @@
 #include <Arduino.h>
+#include <Wire.h> // for I2C
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <RTClib.h>
 
-// put function declarations here:
-int myFunction(int, int);
+Adafruit_MPU6050 accel;
+RTC_DS3231 gyro;
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+void setup() 
+{
+  Serial.begin(115200);
+  Serial.println("Tracker Started");
+
+  Wire.begin(); // Add pins?
+
+  if (accel.begin() == false)
+  {
+    Serial.println("MPU6050 Failed to Start");
+    return;
+  }
+  Serial.println("MPU6050 Started");
+
+  if (gyro.begin() == false)
+  {
+    Serial.println("DS3231 Failed to Start");
+    return;
+  }
+  Serial.println("DS3231 Started");
+
+  if (gyro.lostPower()) 
+  {
+    Serial.println("RTC lost power, setting the time.");
+    gyro.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  }
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop() 
+{
+  // Next Steps: ask GPT for documentation on these libraries so that I can try to use the proper methods myself
+  // Think about what data I want to collect and more importantly HOW TO DISPLAY IT on the phone
 }
